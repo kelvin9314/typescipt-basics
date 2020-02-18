2db37f3269827d889e128a112b9e23ea2169d991
kelvin9314 <kelvin9314@gmail.com>
2020-02-19 07:57:00 +0800

    [types] enum

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-var person = {
-    // const person =  {
-    name: 'Kelvin',
-    age: 20,
-    hobbies: ['sports', 'cooking'],
-    role: [2, 'author']
-};
-console.log(person.name);
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'Kelvin',
+//   age: 20,
+//   hobbies: ['sports', 'cooking'],
+//   role: [2, 'author']
+// }
 // person.role.push('admin')
 // person.role[1] = 10  //ERROR
 // person.role = [0, 'admin', 'user'] //ERROR
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'Kelvin',
+    age: 20,
+    hobbies: ['sports', 'cooking'],
+    role: Role.ADMIN
+};
+if (person.role === Role.ADMIN)
+    console.log('Is admin');
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
