e99f79616cd8ac83820cfedbe18762b4e5605a5a
kelvin9314 <kelvin9314@gmail.com>
2020-02-19 07:37:33 +0800

    [types] Tuple

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-// const person: {
-//   name: string,
-//   age:  number 
-// } = {
 var person = {
+    // const person =  {
     name: 'Kelvin',
     age: 20,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
 console.log(person.name);
+// person.role.push('admin')
+// person.role[1] = 10  //ERROR
+// person.role = [0, 'admin', 'user'] //ERROR
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.localeCompare()); // it will case ERROR !
 }
--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
-// const person: {
-//   name: string,
-//   age:  number 
-// } = {
-const person =  {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+// const person =  {
   name: 'Kelvin',
   age: 20,
-  hobbies: ['sports', 'cooking']
+  hobbies: ['sports', 'cooking'],
+  role: [2, 'author']
 }
 
 console.log(person.name);
+
+// person.role.push('admin')
+// person.role[1] = 10  //ERROR
+// person.role = [0, 'admin', 'user'] //ERROR
+
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
