f5c817a83fbc604227eef3758af8089fcb039b18
kelvin9314 <kelvin9314@gmail.com>
2020-02-19 07:18:02 +0800

    [types] object

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-var add = function (n1, n2, showResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //   throw new Error('Incorrect input!');
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+var person = {
+    name: 'Kelvin',
+    age: 20
 };
-var number1;
-number1 = '5'; // 5.0
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-resultPhrase = 0;
-add(number1, number2, printResult, resultPhrase);
+// const person: {
+//   name: string,
+//   age:  number
+// } = {
+//   name: 'Kelvin',
+//   age: 20
+// }
+console.log(person.name);
