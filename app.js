3b54c401ec65f0150c90ee66064e43a56049e2cc
kelvin9314 <kelvin9314@gmail.com>
2020-02-20 07:26:27 +0800

    [types] Literal

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var combine = function (input1, input2) {
+var combine = function (input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number')
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number')
+        result = +input1 + +input2;
     else
         result = input1.toString() + input2.toString();
     return result;
 };
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
-var combinedName = combine('Max', 'Anna');
+var combineStringAges = combine('30', '26', 'as-number');
+console.log(combineStringAges);
+var combinedName = combine('Max', 'Anna', 'as-text');
 console.log(combinedName);
--- app.ts
+++ app.ts
@@ -1,14 +1,21 @@
-const combine = (input1: number | string, input2: number | string) => {
+const combine = (
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) => {
   let result;
-  if(typeof input1 === 'number' && typeof input2 ==='number')
-    result = input1 + input2;
+  if(typeof input1 === 'number' && typeof input2 ==='number' || resultConversion === 'as-number')
+    result = +input1 + +input2;
   else
     result = input1.toString() + input2.toString()
   return result
 };
 
-const combinedAges = combine(30, 26)
+const combinedAges = combine(30, 26, 'as-number')
 console.log(combinedAges);
 
-const combinedName = combine('Max', 'Anna')
+const combineStringAges = combine('30', '26', 'as-number')
+console.log(combineStringAges);
+
+const combinedName = combine('Max', 'Anna', 'as-text')
 console.log(combinedName);
