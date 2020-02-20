3b39386edfbd0a801f3d2b214d922e9985e19107
kelvin9314 <kelvin9314@gmail.com>
2020-02-20 08:06:29 +0800

    Functions as Types

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ function printResult(num) {
     console.log('Result: ' + num);
 }
 printResult(add(5, 12));
+// JS
+// let combineValues;
+// combineValues = add
+// combineValues = 5
+// console.log(combineValues(8,8));
+var combineValues;
+combineValues = add;
+combineValues = printResult;
+console.log(combineValues(8, 8));
--- app.ts
+++ app.ts
@@ -6,4 +6,17 @@ function printResult(num: number): void {
   console.log('Result: ' +  num);
 }
 
-printResult(add(5,12))
+printResult(add(5, 12))
+
+// JS
+// let combineValues;
+// combineValues = add
+// combineValues = 5
+// console.log(combineValues(8,8));
+
+// it accepts any function with two arug and both arug are number type
+let combineValues: (a: number, b: number) => number;
+combineValues = add
+// combineValues = printResult
+
+console.log(combineValues(8,8));
