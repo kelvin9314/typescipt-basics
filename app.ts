bb575ee7bd2cbd49a25031d4db1a161afe03baab
kelvin9314 <kelvin9314@gmail.com>
2020-02-21 07:55:40 +0800

    [type] unknow

--- app.ts
+++ app.ts
@@ -1,31 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2
+let userInput: unknown
+let userName: string
+
+userInput = 5
+userInput = 'Max'
+// userName = userInput
+if (typeof userInput === 'string') {
+  userName = userInput
 }
-
-function printResult(num: number): void {
-  console.log('Result: ' +  num);
-}
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void ) {
-  const result = n1 + n2
-  cb(result)
-}
-
-
-printResult(add(5, 12))
-
-// JS
-// let combineValues;
-// combineValues = add
-// combineValues = 5
-// console.log(combineValues(8,8));
-
-// it accepts any function with two arug and both arug are number type
-let combineValues: (a: number, b: number) => number;
-combineValues = add
-// combineValues = printResult
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-})
---
+++ functions.ts
@@ -0,0 +1,31 @@
+function add(n1: number, n2: number) {
+  return n1 + n2
+}
+
+function printResult(num: number): void {
+  console.log('Result: ' +  num);
+}
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void ) {
+  const result = n1 + n2
+  cb(result)
+}
+
+
+printResult(add(5, 12))
+
+// JS
+// let combineValues;
+// combineValues = add
+// combineValues = 5
+// console.log(combineValues(8,8));
+
+// it accepts any function with two arug and both arug are number type
+let combineValues: (a: number, b: number) => number;
+combineValues = add
+// combineValues = printResult
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+})
