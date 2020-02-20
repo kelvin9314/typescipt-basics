4b4cca6bf0b0f2dfa713a27476209f9aa3fdeb08
kelvin9314 <kelvin9314@gmail.com>
2020-02-20 08:15:18 +0800

    Function Types & callback

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
 // JS
 // let combineValues;
 // combineValues = add
 // combineValues = 5
 // console.log(combineValues(8,8));
+// it accepts any function with two arug and both arug are number type
 var combineValues;
 combineValues = add;
-combineValues = printResult;
+// combineValues = printResult
 console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -5,6 +5,11 @@ function add(n1: number, n2: number) {
 function printResult(num: number): void {
   console.log('Result: ' +  num);
 }
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void ) {
+  const result = n1 + n2
+  cb(result)
+}
+
 
 printResult(add(5, 12))
 
@@ -19,4 +24,8 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add
 // combineValues = printResult
 
-console.log(combineValues(8,8));
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+})
