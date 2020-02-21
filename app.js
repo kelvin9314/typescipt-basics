7a3ca2e701922b4cda9ed00c989008b3e525d474
kelvin9314 <kelvin9314@gmail.com>
2020-02-21 08:01:13 +0800

    [type] never

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// userName = userInput
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log('Result: ' + num);
+function generateError(msg, code) {
+    throw { message: msg, errorCode: code };
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-// JS
-// let combineValues;
-// combineValues = add
-// combineValues = 5
-// console.log(combineValues(8,8));
-// it accepts any function with two arug and both arug are number type
-var combineValues;
-combineValues = add;
-// combineValues = printResult
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError('An error occurred!', 500);
--- app.ts
+++ app.ts
@@ -7,3 +7,10 @@ userInput = 'Max'
 if (typeof userInput === 'string') {
   userName = userInput
 }
+
+function generateError(msg: string, code: number): never {
+  throw { message: msg, errorCode: code };
+  // while(true) {}
+}
+
+generateError('An error occurred!', 500)
