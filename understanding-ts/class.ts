a1814b20aaa08c41a1f920597d4aab62dac93a5d
kelvin9314 <kelvin9314@gmail.com>
2020-12-02 00:20:00 +0800

    "private" & "public" Access Modifiers

--- understanding-ts/class.ts
+++ understanding-ts/class.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = []
 
   constructor(n: string) {
     this.name = n
@@ -8,16 +9,29 @@ class Department {
   describe(this: Department){
     console.log(`Department: ${this.name}`)
   }
+
+  addEmployees(employee: string){
+    this.employees.push(employee)
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
 }
 
 const accounting = new Department('Accounting')
 console.log(accounting.name)
+accounting.addEmployees('Max')
+accounting.addEmployees('Hugo')
 
 accounting.describe()
+
+// accounting.printEmployeeInfo()
 
 
 // const accountingCopy = {describe: accounting.describe}
 // accountingCopy.describe()  //  it return undefined
 
-const accountingCopy2 = {name: 'copy', describe: accounting.describe}
-accountingCopy2.describe()  //  it return undefined
+// const accountingCopy2 = {name: 'copy', describe: accounting.describe}
+// accountingCopy2.describe()  //  it return undefined
--- understanding-ts/js-compile/class.js
+++ understanding-ts/js-compile/class.js
@@ -1,16 +1,24 @@
 "use strict";
 var Department = (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log("Department: " + this.name);
     };
+    Department.prototype.addEmployees = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInfo = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department('Accounting');
 console.log(accounting.name);
+accounting.addEmployees('Max');
+accounting.addEmployees('Hugo');
 accounting.describe();
-var accountingCopy2 = { name: 'copy', describe: accounting.describe };
-accountingCopy2.describe();
 //# sourceMappingURL=class.js.map
