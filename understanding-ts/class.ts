c0826d2cfed9efdbbff77792b563a095d4ae9cee
kelvin9314 <kelvin9314@gmail.com>
2020-12-01 23:34:19 +0800

    constructor Func & The "this" Keyword

--- understanding-ts/class.ts
+++ understanding-ts/class.ts
@@ -4,8 +4,20 @@ class Department {
   constructor(n: string) {
     this.name = n
   }
+
+  describe(this: Department){
+    console.log(`Department: ${this.name}`)
+  }
 }
 
 const accounting = new Department('Accounting')
-
 console.log(accounting.name)
+
+accounting.describe()
+
+
+// const accountingCopy = {describe: accounting.describe}
+// accountingCopy.describe()  //  it return undefined
+
+const accountingCopy2 = {name: 'copy', describe: accounting.describe}
+accountingCopy2.describe()  //  it return undefined
--- understanding-ts/js-compile/class.js
+++ understanding-ts/js-compile/class.js
@@ -3,8 +3,14 @@ var Department = (function () {
     function Department(n) {
         this.name = n;
     }
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
     return Department;
 }());
 var accounting = new Department('Accounting');
 console.log(accounting.name);
+accounting.describe();
+var accountingCopy2 = { name: 'copy', describe: accounting.describe };
+accountingCopy2.describe();
 //# sourceMappingURL=class.js.map
