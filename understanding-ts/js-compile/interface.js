cccd1201de9882388ec5b4d572f4e7fe1bd53e13
kelvin9314 <kelvin9314@gmail.com>
2021-05-25 20:44:56 +0800

    Readonly Interface Properties & Extending Interfaces

--- understanding-ts/js-compile/interface.js
+++ understanding-ts/js-compile/interface.js
@@ -1,9 +1,10 @@
 "use strict";
 var Person = (function () {
-    function Person(n) {
+    function Person(n, p) {
         this.age = 30;
         this.remark = 'tmp';
         this.name = n;
+        this.phone = p;
     }
     Person.prototype.greet = function (phrase) {
         console.log(phrase + ' ' + this.name);
@@ -11,7 +12,7 @@ var Person = (function () {
     return Person;
 }());
 var user1;
-user1 = new Person('Kelvin');
+user1 = new Person('Kelvin', '091234596748');
 user1.greet('Hi there, I am - ');
 console.log(user1);
 //# sourceMappingURL=interface.js.map
--- understanding-ts/src/interface.ts
+++ understanding-ts/src/interface.ts
@@ -1,6 +1,11 @@
-interface Greetable {
-  name: string;
+interface Phone {
+  readonly phone: string,
+}
+interface Named {
+  readonly name: string;
+}
 
+interface Greetable extends Named, Phone {
   greet(phrase: string): void
 }
 
@@ -10,11 +15,13 @@ interface Another {
 
 class Person implements Greetable, Another {
   name: string;
+  phone: string;
   age = 30;
   remark = 'tmp'
 
-  constructor(n: string){
+  constructor(n: string, p: string){
     this.name = n
+    this.phone  = p
   }
 
   greet(phrase: string){
@@ -24,7 +31,10 @@ class Person implements Greetable, Another {
 
 let user1: Greetable ;
 
-user1 = new Person('Kelvin')
+user1 = new Person('Kelvin', '091234596748')
+
+//  it will cause error
+// user1.name = 'change here'
 
 user1.greet('Hi there, I am - ')
 console.log(user1)
